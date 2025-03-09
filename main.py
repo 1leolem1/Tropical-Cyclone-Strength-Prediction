8b071849f7d250ec6b6cdd33604a8e3582d67868
leopauls <leopaul.lemeur@gmail.com>
2025-03-09 20:27:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
-
 file_loc = 'data/ibtracs.csv'
 
-def read_data(file_loc):
-    data = pd.read_csv(file_loc)
-    return data
+def read_data(file_loc, low_memory=False):
+    dataframe = pd.read_csv(file_loc)
+    dataframe = dataframe.drop(index=0)
+    dataframe['ISO_TIME'] = pd.to_datetime(dataframe['ISO_TIME'])
+    return dataframe
 
-print(read_data(file_loc))
+def clean_data(df):
+    df = df[df['ISO_TIME'] < '1990-01-01'] # Filter out data after 1989 (where we have a target variable)
+    return df
+
+
+# code 
+
+df = read_data(file_loc)
+df = clean_data(df)
